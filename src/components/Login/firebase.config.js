86babd5ed3995a20bb98912fcb9187e63e4b5ec3
Md Razikul Islam Joni <mdrazikulislamjoni@gmail.com>
2021-03-27 12:15:22 +0600

    first commit

---
+++ src/components/Login/firebase.config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCFOqC8pJdNbWi0NYndpHmeACbqRW2jwTA",
+    authDomain: "burj-al-arab-b52d3.firebaseapp.com",
+    projectId: "burj-al-arab-b52d3",
+    storageBucket: "burj-al-arab-b52d3.appspot.com",
+    messagingSenderId: "766479701567",
+    appId: "1:766479701567:web:e6b88cab0c1e47438cca8a",
+};
+export default firebaseConfig;
